248b1f4e06c9ed2ba012aadc0c3828d9e4944b8a
lucasparli <107546287+lucasparli@users.noreply.github.com>
2022-06-22 09:08:41 +0200

    Update PDF_to_Excel.py
    Browsing through a specific "head folder" was added
    cleaning up the list for specific characters in list was added

--- PDF_to_Excel.py
+++ PDF_to_Excel.py
@@ -6,5 +6,38 @@ Created on Wed Jun 15 13:03:11 2022
 """
 
 """
-This code converts a PDF to an excel file
+THIS CODE LOOKS THROUGH ALL FOLDERS AND SEARCHES FOR PDF FILES TO BE CONVERTED INTO EXCEL FILES
 """
+import os
+from os import walk
+
+
+#THIS PIECE OF CODE LISTS ALL FILES IN EACH FOLDER, SUBFOLDER, ETC.
+path ="C:/Users\lpa\Desktop\Lehren"
+#we shall store all the file names in this list
+filelist = []
+
+for root, dirs, files in os.walk(path):
+	for file in files:
+        #append the file name to the list
+		filelist.append(os.path.join(root,file))
+
+#print all the file names
+#for name in filelist:
+#    print(name)
+    
+#print(filelist)
+    
+    
+# THIS PIECE OF CODE SORTS ALL FILES THAT CONTAIN "BOM" IN THEIR PATH
+
+shortlist = []
+
+for path in filelist:
+    if "BOM" in path:
+        shortlist.append(path)
+        
+for name in shortlist:
+    print(name)
+        
+#print(shortlist)
